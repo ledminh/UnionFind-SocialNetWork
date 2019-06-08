becec1bf6b77fb54f42f5a1adc10156ea7254e7e
Minh Le <duyminh.le310@gmail.com>
2019-06-07 18:22:11 -0700

    Add JUnit Test and finish UnionFind Alg

--- src/UF.java
+++ src/UF.java
@@ -1,26 +1,64 @@
 
 public class UF {
-	private int [] vertices;
-	private int [] roots;
+	private int [] sizes;
+	private int [] parents;
+	
 	public UF(int [] v) {
-		vertices = v;
+				
+		parents = new int[v.length];
+		sizes = new int[v.length];
 		
-		roots = new int[v.length];
-		
-		for(int i = 0; i < roots.length; i++) {
-			roots[i] = i;
+		for(int i = 0; i < parents.length; i++) {
+			parents[i] = i;
+			sizes[i] = 1;
 		}
 	}
 	
-	public void union() {
+	public void union(int a, int b) {
+		int rA = root(a),
+			rB = root(b);
+		
+		if(sizes[rA] > sizes[rB]) {
+			parents[rB] = rA;
+			sizes[rA] += sizes[rB];
+		}
+		else if(sizes[rA] < sizes[rB]) {
+			parents[rA] = rB;
+			sizes[rB] += sizes[rA];
+		}
+		else {
+			if(rA > rB) {
+				parents[rA] = rB;
+				sizes[rB] += sizes[rA];
+			}
+			else
+				parents[rB] = rA;
+				sizes[rA] += sizes[rB];
+		}
+		
+		
 		
 	}
 	
-	
-	public boolean connected() {
-		boolean isConnected = false;
-		
-		return isConnected;
+	public boolean connected(int a, int b) {
+			
+		return root(a) == root(b);
 	}
+	
+	
+	public int size(int i) {
+		return sizes[root(i)];
+	}
+	
+	
+	private int root(int i) {
+		
+		while(parents[i] != i) {
+			i = parents[i];
+		}
+		
+		return i;
+	}
+	
 	
 }
